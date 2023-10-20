4e532dd959e5855fd6971a30fb0ee4dde20553fc
nyitrai100 <nyitrai100@gmail.com>
2023-10-20 19:50:11 +0100

    main page almost done. started to do the login

---
+++ loginPopUp.js
@@ -0,0 +1,9 @@
+
+    function openLogin() {
+        document.getElementById('login-div').style.display = 'flex';
+    }
+
+    // Function to close the pop-up
+    function closeLogin() {
+        document.getElementById('login-div').style.display = 'none';
+    }
